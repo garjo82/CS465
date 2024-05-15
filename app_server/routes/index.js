df4df9aa1052b3ea4ee482a19ca797d27dcd6507
Joel Garcia <gajoe1982@gmail.com>
2024-05-14 21:23:40 -0700

    Module 2 completed baseline

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const ctrlMain = require('../controllers/main');
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
